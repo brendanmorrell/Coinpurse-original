b0cccf0dad654ca95c5d1067388be196f459f66e
Brendan Morrell <brendanmorrell@gmail.com>
2017-10-21 11:46:51 -0600

    gitignore change

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
   <div>
     <p>This is my EditExpenseComponent!</p>
-  </div>
-);
+    <br />
+    editing expense with id of {props.match.params.id} 
+  </div>)
+};
 
 export default EditExpensePage;
