e9a93510b1bcf035411ad417460f46083ef6aeb6
Brendan Morrell <brendanmorrell@gmail.com>
2017-10-30 18:57:11 -0600

    set up jest and created test cases for everything but the components

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>Expensify!</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>{/*NavLink has a bunch of different props that allow you to manipulate the link in cool ways*/}
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>{/* NavLink has a bunch of different props that allow you to manipulate the link in cool ways */}
     <NavLink to="/create" activeClassName="is-active" exact={true}>Create Expense</NavLink>
     <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
   </header>
